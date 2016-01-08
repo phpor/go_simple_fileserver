26f60e95bc2e914453aba7dd438d7402a166de53
phpor <phpor@users.noreply.github.com>
2016-01-08 14:53:30 +0800

    Update upload.go

--- controller/upload.go
+++ controller/upload.go
@@ -5,3 +5,20 @@ import "net/http"
 func Upload(w http.ResponseWriter, r *http.Request) {
 
 }
+
+/*
+ http/server.go 中有如下关于expect的逻辑，使得即使请求头中含有Expect，req.Header中也看不到
+ 
+		// Expect 100 Continue support
+		req := w.req
+		if req.expectsContinue() {
+			if req.ProtoAtLeast(1, 1) && req.ContentLength != 0 {
+				// Wrap the Body reader with one that replies on the connection
+				req.Body = &expectContinueReader{readCloser: req.Body, resp: w}
+			}
+			req.Header.Del("Expect")
+		} else if req.Header.get("Expect") != "" {
+			w.sendExpectationFailed()
+			break
+		}
+		*/
